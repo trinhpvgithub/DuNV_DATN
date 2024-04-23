d32cd5b6907254eb0a22383d4b721e93f9950e20
trinhhandsome <trinhtrong0505@gmail.com>
2024-04-23 16:39:10 +0700

    update Panel

---
+++ DuNV_DATN/Commands/App.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.Revit.Attributes;
+using Nice3point.Revit.Toolkit.External;
+
+namespace DuNV_DATN.Commands
+{
+	[UsedImplicitly]
+	public class App : ExternalApplication
+	{
+		public override void OnStartup()
+		{
+			CreateRibbon();
+		}
+		private void CreateRibbon()
+		{
+			var panel = Application.CreatePanel("Button", "NVD");
+			var tools = panel.AddPushButton<Command>("CreateColumnSection");
+			//tools.SetImage("/DuNV_DATN;component/Resources/Icons/column_16.png");
+			//tools.SetLargeImage("/DuNV_DATN;component/Resources/Icons/column_32.png");
+		}
+	}
+}
